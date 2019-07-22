c00049c91f366d837e0c1c3e412553ac5e041abe
Alan Fitzpatrick <alan.fitzpatrick@jet.com>
2019-07-22 14:49:11 +0100

    Updating findings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { Machine, interpret } from 'xstate'
+import { Machine, interpret, actions } from 'xstate'
+
+const { log } = actions
 
 const promiseMachine = Machine({ 
     id: 'promise',
@@ -27,3 +29,47 @@ const promiseService = interpret(promiseMachine)
 promiseService.start()
 
 promiseService.send('RESOLVE')
+
+const triggerMachine = Machine({
+    id: 'trigger',
+    initial: 'inactive',
+    states: {
+        inactive: {
+            on: {
+                TRIGGER: {
+                    target: 'active',
+                    actions: ['activate', 'sendTelemetry', log(
+                        (context, event) => `Event: ${event.type}`
+                    )]
+                }
+            }
+        },
+        active: {
+            entry: ['notifyActive', 'sendTelemetry'],
+            exit: ['notifyInactive', 'sendTelemetry'],
+            on: {
+                STOP: 'inactive'
+            }
+        }
+    }
+}, {
+    actions: {
+        activate: (context, event) => {
+            console.log('activating...')
+        },
+        notifyActive: (context, event) => {
+            console.log('active')
+        },
+        notifyInactive: (context, event) => {
+            console.log('inactive!')
+        },
+        sendTelemetry: (context, event) => {
+            console.log('time:', Date.now())
+        }
+    }
+})
+
+const triggerService = interpret(triggerMachine)
+triggerService.start()
+triggerService.send('TRIGGER')
+triggerService.send('STOP')
